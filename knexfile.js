8d092e88b77399dd02f3da2105c7a05c000ca968
mayl730 <maylaixyz@gmail.com>
2022-05-17 23:54:46 +0900

    fix: change configs for deployment

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ require("dotenv").config({
  */
 module.exports = {
   client: "postgresql",
-  connection: process.env.DB_URL || {
+  connection: process.env.DATABASE_URL || {
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
