ce470a8b8e012a7b17d20889ae4935cd9e4e5a15
mayl730 <maylaixyz@gmail.com>
2022-05-20 20:16:11 +0900

    afix: change get by calories API to return pure array of objects

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -16,13 +16,7 @@ router.get("/", async (req, res) => {
     if (req.query.calories) {
       const { lt, gt } = JSON.parse(req.query.calories);
       recipes = await Recipe.findByCalories(lt, gt);
-      return res
-        .status(200)
-        .send([
-          `Recipe with calories less than ${lt} and greater than ${gt}:`,
-          recipes,
-        ])
-        .end();
+      return res.status(200).send(recipes).end();
     }
 
     const recipe = await Recipe.findMany();
