23717231aa683716be793b6565780d1d94228207
mayl730 <maylaixyz@gmail.com>
2022-05-21 14:32:53 +0900

    seed: seed data again

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -4,7 +4,7 @@ const Recipe = require("../models/Recipe");
 const router = express.Router();
 
 router.use(express.json());
-// Get Alll
+// Get Allll
 router.get("/", async (req, res) => {
   try {
     // Get First X Items
