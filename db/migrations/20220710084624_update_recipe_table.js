3a4db08144c91581da153e11758fc902c2cf6acc
mayl730 <maylaixyz@gmail.com>
2022-07-10 18:17:45 +0900

    migration: add image field to recipe table

---
+++ db/migrations/20220710084624_update_recipe_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("recipe", function (table) {
+    table.string("image", 400);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("recipe", function (table) {
+    table.dropColumn("image");
+  });
+};
