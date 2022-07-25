b9a374e4f288e5fb28dba66458374d457a0f47d7
mayl730 <maylaixyz@gmail.com>
2022-07-26 00:00:29 +0900

    migration: add instruction column to recipe

---
+++ db/migrations/20220725145824_update_recipe_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("recipe", function (table) {
+    table.string("instruction", 1000);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("recipe", function (table) {
+    table.dropColumn("instruction");
+  });
+};
